60a0187e5605608b9bf98d2029e5c07bf04e5e87
BAUMANN SOPHIE <sophie.b.baumann@gmail.com>
2024-06-06 11:38:26 +0200

    Wrote a first program that collects urls for "Data Analyist" job offers, sorted by date, and saves them in a dataframe

--- scraping/make_sense.py
+++ scraping/make_sense.py
@@ -1,14 +1,47 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+import re
+import sys  # to be able to exit the program in case of errors, using sys.exit() to stop the program
 
-# sorting by date
+
+"""creating a dataframe with 1 column: url of each job offer"""
+
+# sorting by date "cratedAT"
 url = "https://jobs.makesense.org/fr/s/jobs/all?s=Data%20Analyst&sortBy=createdAt"
-html = requests.get(url)
+
+# in case the site is protected
+navigator = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'  
+html = requests.get(url, headers={'User-Agent': navigator})
 
 soup = BeautifulSoup(html.text, "html.parser")
 
-jobs = soup.find_all("div", {"class": "job__container"})
+# error handling: status code must be 200 (request worked)
+if html.status_code != 200:
+    sys.exit("could not access url, check request")
 
-print(jobs[0])
+# finding all jobs listed on the page
+soup = BeautifulSoup(html.text, "html.parser")
+jobs = soup.find_all("div", {"class": "job"})
 
+# error handling in case jobs is empty
+if len(jobs) == 0:
+    sys.exit("no jobs found, check your find_all")
+
+# looping through joubs and saving each url in a df
+pattern = r'href="(.*?)"'
+
+url_list = []
+
+for job in jobs:
+    match = re.search(pattern, str(job))
+
+    # error handling: in case of problems finding the link
+    try:
+        url = "https://jobs.makesense.org/" + match.group(1)
+    except AttributeError:
+        sys.exit("can't find job urls")
+
+    url_list.append(url)
+
+df_url = pd.DataFrame(url_list, columns=["url"])
