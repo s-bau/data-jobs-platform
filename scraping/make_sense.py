b4188be26c760ec11add9d44357c0a90c92c9353
BAUMANN SOPHIE <sophie.b.baumann@gmail.com>
2024-06-10 16:02:28 +0200

    Put all the code from make_sense_2 into make_sense

--- scraping/make_sense.py
+++ scraping/make_sense.py
@@ -68,54 +68,132 @@ for item in urls_to_scrape:
             sys.exit("can't find job urls")
 
 
-        # title
+        # job title
         try:
-            title = job.find("h3", {"class": "content__title"}).text.strip()
+            name = job.find("h3", {"class": "content__title"}).text.strip()
         except AttributeError:
-            title = None
+            name = None
 
-        # employer
-        employer = job.find("div", {"class": "meta"}).text.strip()
+        # Nom entreprise
+        organization_reference = job.find("div", {"class": "meta"}).text.strip()
 
-        # employer info
-        info = job.find("p", {"class": "content__project-mission"}).text.strip()
+        # Description entreprise
+        company_description = job.find("p", {"class": "content__project-mission"}).text.strip()
 
         # details
         details = str(job.find_all("div", {"class": "meta"}))
 
-        # sense
+        # impact (why this job "makes sense")
         try:
             dpattern = r"</span>(.*?)<!"
             match = re.search(dpattern, details, re.DOTALL)
-            sense = match.group(1).strip()
+            impact = match.group(1).strip()
         except AttributeError:
-            sense = None
+            impact = None
     
-        # type of contract
+        # Type de contrat
         try:
             cpattern = r"<!-- -->((?:(?![<>]).)*?)<!-- -->"
             match = re.search(cpattern, details, re.DOTALL)
-            contract = match.group(1).strip()
+            contract_type = match.group(1).strip()
         except AttributeError:
-            contract = None
+            contract_type = None
     
-        # location
+        # Localisation
         try:
             lpattern = r"</svg>((?:(?![<>]).)*?)</address>"
             match = re.search(lpattern, details, re.DOTALL)
-            location = match.group(1).strip()
+            city = match.group(1).strip()
         except AttributeError:
-            location = None
+            city = None
     
 
         # all job details
         detail = {"url": url,
-                "title": title,
-                "employer": employer,
-                "info": info,
-                "sense": sense,
-                "contract": contract,
-                "location": location}
+                "organization_reference": organization_reference,
+                "name": name,
+                "company_description": company_description,
+                "impact": impact,
+                "contract_type": contract_type,
+                "city": city}
         details_list.append(detail)
 
-df_details = pd.DataFrame(details_list, columns=["url", "title", "employer", "info", "sense", "contract", "location"])
+df_details = pd.DataFrame(details_list, columns=["url",
+                                                 "organization_reference",
+                                                 "name",
+                                                 "company_description",
+                                                 "impact",
+                                                 "contract_type",
+                                                 "city"])
+
+
+"""accessing infos from each page"""
+
+additional_list = []
+
+for value in df_details["url"].values:
+    
+    url = value
+
+    navigator = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'  
+    html = requests.get(url, headers={'User-Agent': navigator})
+
+    soup = BeautifulSoup(html.text, "html.parser")
+
+    section = soup.find("section", {"class": "meta section--style-default"})
+
+    # publication date
+    date_pattern = r"\b\d{2}/\d{2}/\d{4}\b"
+    match = re.search(date_pattern, str(section))
+    try:
+        publication_date = match.group()
+    except AttributeError:
+        publication_date = None
+    
+    # salaire
+    salaire_pattern = r"</svg>(\s*\d[\s\S]*?\s*)</div>"
+    match = re.search(salaire_pattern, str(section), re.DOTALL)
+
+    if 'title id="coffee"' in str(section):
+        try:
+            salaire = match.group(1).strip()
+        except AttributeError:
+            salaire = None
+    else:
+        salaire = None
+
+    # début du contrat
+    debut_pattern = r"Début\s*:\s*((?:(?![\n]).)*?)\n"
+    match = re.search(debut_pattern, str(section), re.DOTALL)
+    try:
+        start_date = match.group(1)
+    except AttributeError:
+        start_date = None
+    
+    # missions
+    try:
+        missions = soup.find("main", {"class": "job__main-content"}).text
+    except AttributeError:
+        missions = None
+
+    # profil
+    try:
+        profil = soup.find("div", {"class": "job__main-content"}).text
+    except AttributeError:
+        profil = None
+    
+
+    additional = {"url": url,
+                  "publication_date": publication_date,
+                  "salaire": salaire,
+                  "start_date": start_date,
+                  "missions": missions,
+                  "profil": profil}
+    additional_list.append(additional)
+
+df_additional = pd.DataFrame(additional_list, columns=["url", "publication_date", "salaire", "start_date", "missions", "profil"])
+
+df_make_sense = pd.merge(df_details, df_additional, on="url")
+
+# save as csv
+df_make_sense.to_csv("df_make_sense.csv", sep=",", encoding="utf-8", index=False)
